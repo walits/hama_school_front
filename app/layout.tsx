88f66e290fd08727e619e5b04ff4112b88b5478e
lee <hama@mac.local>
2026-03-31 15:39:32 +0900

    Add Google Ads conversion tracking tag (AW-18053121883)

--- app/layout.tsx
+++ app/layout.tsx
@@ -77,6 +77,19 @@ export default function RootLayout({
           crossOrigin="anonymous"
           strategy="afterInteractive"
         />
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-18053121883"
+          strategy="afterInteractive"
+        />
+        <Script id="google-ads-gtag" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-18053121883');
+          `}
+        </Script>
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
